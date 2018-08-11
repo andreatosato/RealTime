2f7d3ffe10d956e4b3c1ada59a3962282f76f0eb
Andrea Tosato <andrea.tosato@hotmail.it>
2018-08-11 09:33:14 +0200

    ChatHub Connection Notifications

--- ChatPWA/ChatApp.Backend/ChatApp.Backend/Hubs/ChatHub.cs
+++ ChatPWA/ChatApp.Backend/ChatApp.Backend/Hubs/ChatHub.cs
@@ -13,7 +13,9 @@ namespace ChatApp.Backend.Hubs
     {
         public override Task OnConnectedAsync()
         {
-            ChatStore.UsersOnline.Add(new UserSignalR(Context.User.Identity.Name, Context.ConnectionId));
+            var currentUser = new UserSignalR(Context.User.Identity.Name, Context.ConnectionId);
+            ChatStore.UsersOnline.Add(currentUser);
+            NewConnectedUser(currentUser);
             return base.OnConnectedAsync();
         }
 
@@ -21,24 +23,56 @@ namespace ChatApp.Backend.Hubs
         {
             var currentUser = ChatStore.UsersOnline.Find(x => x.Username == Context.User.Identity.Name);
             if(currentUser != null)
+            {
                 ChatStore.UsersOnline.Remove(currentUser);
-
+                RemoveFromGroup();
+            }
             return base.OnDisconnectedAsync(exception);
         }
 
-        public UserSignalR GetUserContext()
-        {
-            return new UserSignalR(Context.User.Identity.Name, Context.ConnectionId);
-        }
-
+        #region SignalR
         public void AddPrivateMessage(MessageModel message)
         {
             Clients.Client(GetConnectionId(message.To.Username)).SendAsync("ReceivePrivateMessage", message);
+        }
+
+        private void NewConnectedUser(UserSignalR currentUser)
+        {
+            Clients.Others.SendAsync("NewConnectedUser", currentUser);
+        }
+        #endregion
+
+        #region Utility
+        public UserSignalR GetUserContext()
+        {
+            return new UserSignalR(Context.User.Identity.Name, Context.ConnectionId);
         }
 
         private string GetConnectionId(string username)
         {
             return ChatStore.UsersOnline.FirstOrDefault(x => x.Username == username).ConnectionId;
         }
+
+        private void RemoveFromGroup()
+        {
+            // Remove Disconnected User
+            for (int i = 0; i < ChatStore.UsersByGroups.Count; i++)
+            {
+                var currentGroup = ChatStore.UsersByGroups.ElementAt(i);
+                var userToRemove = currentGroup.Users.FirstOrDefault(t => t.Username == Context.User.Identity.Name);
+                if (userToRemove != null)
+                {
+                    currentGroup.Users.Remove(userToRemove);
+                }
+            }
+
+            // Remove Empty Group
+            var groupsToRemove = ChatStore.UsersByGroups.Where(x => !x.Users.Any());
+            for (int i = 0; i < groupsToRemove.Count(); i++)
+            {
+                ChatStore.UsersByGroups.Remove(groupsToRemove.ElementAt(i));
+            }
+        }
+        #endregion
     }
 }
